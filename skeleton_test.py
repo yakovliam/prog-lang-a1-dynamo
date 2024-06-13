be0cb0e62acd5742d02c7b56718a7c85b5179221
yakovliam <jacob@yakovliam.com>
2024-06-13 00:09:14 -0400

    feat: update

--- skeleton_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import skeleton_test
-
-if __name__ == "__main__":
-    suite = unittest.findTestCases(skeleton_test)
-    result = unittest.TestResult()
-    suite.run(result, debug=True)
-    print(f"{result=}")
